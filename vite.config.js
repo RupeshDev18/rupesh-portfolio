f71375daba9585c2a370388f037d3a5879231a98
RupeshDev18 <ry993494787@gmail.com>
2024-12-25 00:13:43 +0530

    Add SEO

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import ViteSitemap from 'vite-plugin-sitemap';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [ViteSitemap({
+    hostname: 'https://rupesh-portfolio-tk3v.onrender.com',
+    exclude: ["/googleaaa5db7019bf9846"],
+  }), react()],
 });
