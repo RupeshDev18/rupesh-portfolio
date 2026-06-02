450da528949877ab0c4dcea427305a9b166029d5
Rupesh <112386480+RupeshDev18@users.noreply.github.com>
2026-06-02 04:55:16 +0000

    fix: sitemap couldn't reach

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,11 @@ import react from "@vitejs/plugin-react";
 import ViteSitemap from 'vite-plugin-sitemap';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [ViteSitemap({
-    hostname: 'https://rupesh-yadav.fun',
-  }), react()],
+  plugins: [
+  //   ViteSitemap({
+  //   hostname: 'https://rupesh-yadav.fun',
+  // }),
+   react()],
   server: {
     port: 5173,
     open: true,
